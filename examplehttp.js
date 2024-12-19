6b7706e9a6f5a1e67aacd920947b1e36179964bb
Çınar Kaya <cnrkaya008@gmail.com>
2024-12-19 23:23:53 +0300

    initial commit

---
+++ examplehttp.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+const server = http.createServer((req,res)=>{
+
+    console.log('${req.method} request at ${req.url}');
+
+    res.setHeader('Content-Type', 'text/plain');
+    if(req.url === '/'){
+        res.statusCode = 200;
+        res.end('visting request has been welcommed');
+    }else{
+        res.statusCode = 404;
+        res.end('page not found');
+    }
+});
+
+server.listen(3000, ()=>{console.log('http://localhost:3000/');});
