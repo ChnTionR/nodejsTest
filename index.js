e7370851cd7d51276d1769d2703cb4f992ed1573
Çınar Kaya <cnrkaya008@gmail.com>
2024-12-20 21:13:03 +0300

    this should be the final commit

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ const http = require('http');
 
 const server = http.createServer((req,res)=>{
 
-    console.log('${req.method} request at ${req.url}');
+    console.log(req.method +' request at ' + req.url);
 
     res.setHeader('Content-Type', 'text/plain');
-    if(req.url === './'){
+    if(req.url === '/'){
         res.statusCode = 200;
         res.end('visting request has been welcommed');
     }else{
         res.statusCode = 404;
-        writeHead(404, {'Content-Type':'text-plain'});
         res.end('page not found');
     }
 });
